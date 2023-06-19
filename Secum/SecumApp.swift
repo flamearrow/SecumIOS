3569e73c41a2e85d47ccdbd7f05524f71fa5b555
Chen Cen <arrow.cen@gmail.com>
2023-06-19 12:54:14 -0700

    Initial Commit

---
+++ Secum/SecumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecumApp.swift
+//  Secum
+//
+//  Created by Chen Cen on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SecumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
